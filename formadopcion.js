a57243337f21d48ec73e759e046fa72ba8e01fed
juanmeneses0310 <162385270+juanmeneses0310@users.noreply.github.com>
2024-04-15 22:17:05 -0500

    inclusión de formularios

---
+++ formadopcion.js
@@ -0,0 +1,107 @@
+"use strict";
+
+const $nombre = document.getElementById("nombre");
+const $identificacion = document.getElementById("identificacion");
+const $correo = document.getElementById("correo");
+const $telefono = document.getElementById("telefono");
+const $direccion = document.getElementById("direccion");
+const $barrio = document.getElementById("barrio");
+const $ciudad = document.getElementById("ciudad");
+const $tipoVivienda = document.getElementById("tipoVivienda");
+const $vivienda = document.getElementById("vivienda");
+const $dueño = document.getElementById("dueño");
+const $ocupacion = document.getElementById("nombre");
+const $ingresos = document.getElementById("nombre");
+const $empresa = document.getElementById("nombre");
+const $terminos = document.getElementById("terminos");
+const $enviar = document.getElementById("enviar");
+
+let nombre = null;
+let identificacion = null;
+let correo = null;
+let telefono = null;
+let direccion = null;
+let barrio = null;
+let ciudad = null;
+let tipoVivienda = null;
+let vivienda = null;
+let dueño = null;
+let ocupacion = null;
+let ingresos = null;
+let empresa = null;
+let terminos = null;
+
+$nombre.addEventListener("input", (evento) => {
+  nombre = evento.target.value;
+});
+
+$identificacion.addEventListener("input", (evento) => {
+  identificacion = evento.target.value;
+});
+
+$correo.addEventListener("input", (evento) => {
+  correo = evento.target.value;
+});
+
+$telefono.addEventListener("input", (evento) => {
+  telefono = evento.target.value;
+});
+
+$direccion.addEventListener("input", (evento) => {
+  direccion = evento.target.value;
+});
+
+$barrio.addEventListener("input", (evento) => {
+  barrio = evento.target.value;
+});
+
+$ciudad.addEventListener("input", (evento) => {
+  ciudad = evento.target.value;
+});
+
+$tipoVivienda.addEventListener("input", (evento) => {
+  tipoVivienda = evento.target.value;
+});
+
+$vivienda.addEventListener("input", (evento) => {
+  vivienda = evento.target.value;
+});
+
+$dueño.addEventListener("input", (evento) => {
+  dueño = evento.target.value;
+});
+
+$ocupacion.addEventListener("input", (evento) => {
+  ocupacion = evento.target.value;
+});
+
+$ingresos.addEventListener("input", (evento) => {
+  ingresos = evento.target.value;
+});
+
+$empresa.addEventListener("input", (evento) => {
+  empresa = evento.target.value;
+});
+
+$terminos.addEventListener("input", (evento) => {
+  terminos = evento.target.value;
+});
+
+$enviar.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  console.log(nombre);
+  console.log(identificacion);
+  console.log(correo);
+  console.log(telefono);
+  console.log(direccion);
+  console.log(barrio);
+  console.log(ciudad);
+  console.log(tipoVivienda);
+  console.log(vivienda);
+  console.log(dueño);
+  console.log(ocupacion);
+  console.log(ingresos);
+  console.log(empresa);
+  console.log(terminos);
+  alert("Tus datos fueron recibidos!");
+});
---
+++ formcontactanos.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const $nombre = document.getElementById("nombre");
+const $telefono = document.getElementById("telefono");
+const $email = document.getElementById("email");
+const $edad = document.getElementById("edad");
+const $mensaje = document.getElementById("mensaje");
+const $botonEnviar = document.getElementById("botonEnviar");
+
+let nombre = null;
+let telefono = null;
+let email = null;
+let edad = null;
+let mensaje = null;
+
+$nombre.addEventListener("input", (evento) => {
+  nombre = evento.target.value;
+});
+
+$telefono.addEventListener("input", (evento) => {
+  telefono = evento.target.value;
+});
+
+$email.addEventListener("input", (evento) => {
+  email = evento.target.value;
+});
+
+$edad.addEventListener("input", (evento) => {
+  edad = evento.target.value;
+});
+
+$mensaje.addEventListener("input", (evento) => {
+  mensaje = evento.target.value;
+});
+
+$botonEnviar.addEventListener("click", (evento) => {
+  evento.preventDefault();
+  console.log(nombre);
+  console.log(telefono);
+  console.log(email);
+  console.log(edad);
+  console.log(mensaje);
+  alert("Tus datos fueron recibidos!");
+});
